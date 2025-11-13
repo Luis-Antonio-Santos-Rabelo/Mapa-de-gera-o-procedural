81f3833f3d850def545a3df659dc9652e629e607
ThonyIsNotAvaliable <aggr5555@gmail.com>
2025-11-13 17:56:45 -0300

    Etapa 1 quase feita

---
+++ src/paleta/paleta.h
@@ -0,0 +1,99 @@
+#include "../sequencia/sequencia.h"
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+struct Cor {
+  unsigned char r, g, b;
+};
+
+class Paleta {
+  Sequencia<Cor> paletaInterna;
+  int tamanho;
+
+  unsigned char hexStringToInt(std::string input) {
+    unsigned char sum = 0;
+    
+    for(int i = 0; i < input.size(); i++) {
+      unsigned char c = input[i];
+      unsigned char stepVal = (std::pow(16, (1 - i)));
+      
+      if (c > 47 && c < 58){
+        sum += (c-48) * stepVal;
+      
+      } else if (c > 96 && c < 103) {
+        sum += (c-87) * stepVal;
+        
+      }
+    } 
+
+    return sum;
+  }
+
+  public:
+    Paleta() :tamanho(0), paletaInterna(Sequencia<Cor>(0)) {}
+
+    Paleta(std::string path) {
+      std::ifstream input;
+      std::string tmpString = "";
+      int lineCount = 0;
+
+      input.open(path);
+
+      if(!input.is_open()) {
+        std::cerr << "Erro abrindo o arquivo";
+        //paletaInterna = Sequencia<Cor>(1);
+        tamanho = 0;
+        
+      } else {
+
+        Sequencia<std::string> lineArray = Sequencia<std::string>(10);
+
+        while (std::getline(input, tmpString)) {
+          lineArray.adicionar(tmpString);
+        }
+      
+        paletaInterna = Sequencia<Cor>(lineArray.obterTamanho());
+      
+
+        for (int i = 0; i < lineArray.obterTamanho(); i++) {
+          Cor tmpC = Cor {
+            hexStringToInt(lineArray[i].substr(1, 2)),
+            hexStringToInt(lineArray[i].substr(3, 2)),
+            hexStringToInt(lineArray[i].substr(5, 2)),
+          };
+
+          paletaInterna.adicionar(tmpC);     
+        }
+
+        tamanho = paletaInterna.obterTamanho();
+      }
+      input.close();
+    }
+    
+    int obterTamanho(){
+      return tamanho;
+    };
+
+
+    void adicionarCor(Cor cor) {
+      paletaInterna.adicionar(cor);
+      tamanho = paletaInterna.obterTamanho();
+    }
+
+    Cor obterCor(int index) {
+      if (index > -1 && index < tamanho) {
+        return paletaInterna[index];    
+      }
+
+      return Cor {0, 0, 0};
+    
+    }
+
+    
+    
+    
+};
+
+
---
+++ src/sequencia/sequencia.h
@@ -0,0 +1,71 @@
+
+
+#include <iostream>
+
+
+template<typename T>
+class Sequencia {
+  int capacidade;
+  int tamanho;
+  T *dados;
+
+
+
+
+  void ajustaCapacidade() {
+    capacidade++;
+    capacidade *= 2;
+    T *novo = new T[capacidade];
+    for (int i = 0; i < tamanho; i++) {
+      novo[i] = dados[i];
+    }
+    delete[] dados;
+    dados = novo;
+   }
+
+public:
+  ~Sequencia() {
+    delete[] dados;
+  }
+
+  Sequencia(int cap = 2) : capacidade(cap), tamanho(0) {
+    dados = new T[capacidade];
+  }
+
+  int obterTamanho() {
+    return tamanho;
+  }
+
+  void adicionar(T valor) {
+    if (tamanho < capacidade){
+      dados[tamanho] = valor;
+      tamanho++;
+      
+    } else {
+      ajustaCapacidade();
+      adicionar(valor);
+    }
+  }
+
+  T obterIndice(int indice) {
+    return dados[indice];
+  }
+
+  T& operator[](int index) {
+    if (index < tamanho && index >= 0) {
+      return dados[index];
+    }
+    else {
+      return dados[0];
+    }
+  } 
+
+  void removerUltimo() {
+    if (tamanho <= 0) {
+      std::cout << "Error, tamanho menor que 1";
+    } else {
+      tamanho--;
+    }
+  }
+  
+};
